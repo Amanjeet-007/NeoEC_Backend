4187738255d48090b3e574b2249ee9fda64f35e7
Amanjeet <kumaramanjeet951@gmail.com>
2026-06-23 13:28:49 +0530

    product image size expand from 3mb

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,7 +1,7 @@
 import multer from "multer";
 import path from "path";
 
-const maxSize = 1 * 1000 * 1000; // 1MB
+const maxSize = 3 * 1000 * 1000; // 3MB
 
 const storage = multer.memoryStorage();
 
